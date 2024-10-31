28dd842ba6b93315caf121337672fa7f86582666
KingNono1030 <hoejy0821@gmail.com>
2024-11-01 04:21:16 +0900

    [#23] 🔧 exclude lint and prettier configs from tsconfig.json

--- prettier.config.js
+++ prettier.config.js
@@ -12,20 +12,19 @@ module.exports = {
     '@trivago/prettier-plugin-sort-imports',
     'prettier-plugin-tailwindcss',
   ],
-  tailwindConfig: '/tailwind.config.ts',
+  tailwindConfig: 'tailwind.config.ts',
   tailwindFunctions: ['clsx', 'twMerge'],
   importOrder: [
-    '^react(.*)$',           // React 관련 import를 최상단으로
+    '^react(.*)$', // React 관련 import를 최상단으로
     '<THIRD_PARTY_MODULES>', // 외부 모듈
-    '^@/app/api/(.*)$',      // api 폴더
-    '^@/components/(.*)$',   // components 폴더를
-    '^@/queries/(.*)$',      // queries 폴더
-    '^@/hooks/(.*)$',        // hooks 폴더
-    '^@/services/(.*)$',     // services 폴더
-    '^@/utils/(.*)$',        // utils 폴더
-    '^[./]',                 // 상대 경로
+    '^@/app/api/(.*)$', // api 폴더
+    '^@/components/(.*)$', // components 폴더를
+    '^@/queries/(.*)$', // queries 폴더
+    '^@/hooks/(.*)$', // hooks 폴더
+    '^@/services/(.*)$', // services 폴더
+    '^@/utils/(.*)$', // utils 폴더
+    '^[./]', // 상대 경로
   ],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
-};
-
+}
