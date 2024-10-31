508d1edd528ebe2386bf526b4cc4c657cb021987
KingNono1030 <hoejy0821@gmail.com>
2024-11-01 00:33:33 +0900

    [#23] 🔧 update prettier.config.js along to new plugins

--- prettier.config.js
+++ prettier.config.js
@@ -8,5 +8,24 @@ module.exports = {
   useTabs: false,
   bracketSpacing: true,
   arrowParens: 'avoid',
-  plugins: [],
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  tailwindConfig: '/tailwind.config.ts',
+  tailwindFunctions: ['clsx', 'twMerge'],
+  importOrder: [
+    '^react(.*)$',           // React 관련 import를 최상단으로
+    '<THIRD_PARTY_MODULES>', // 외부 모듈
+    '^@/app/api/(.*)$',      // api 폴더
+    '^@/components/(.*)$',   // components 폴더를
+    '^@/queries/(.*)$',        // hooks 폴더
+    '^@/hooks/(.*)$',        // hooks 폴더
+    '^@/services/(.*)$',     // services 폴더
+    '^@/utils/(.*)$',        // utils 폴더
+    '^[./]',                 // 상대 경로
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
+
