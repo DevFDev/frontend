9ff50e137a82d3e5444890cd9af2e701a932dd39
KingNono1030 <hoejy0821@gmail.com>
2024-11-01 00:04:18 +0900

    [#23] 🔧 add Prettier config.js

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  plugins: [],
+};
