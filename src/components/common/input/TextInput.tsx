8af2aec1d9796fd117aaf26a565f40a2eac29a3b
binyongbin <kyb2475@gmail.com>
2024-11-05 16:13:19 +0900

    [#43] ♻️ Refactor TextInput with tailwind-merge and update baseStyles

--- src/components/common/input/TextInput.tsx
+++ src/components/common/input/TextInput.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx'
 import { ReactNode } from 'react'
 import { FieldValues, UseFormRegister } from 'react-hook-form'
+import { twMerge } from 'tailwind-merge'
 
 interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: boolean
@@ -12,7 +13,7 @@ interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 const baseStyles =
-  'h-52 p-14 focus:border-primary-normal rounded-lg border-1 border-gray-200 placeholder:text-body2 placeholder:font-medium placeholder:text-gray-500'
+  'h-52 p-14 focus:border-primary-normal rounded-lg border-1 border-gray-200 text-body2 leading-body2 font-medium placeholder:text-gray-500'
 const errorStyles = 'border-semantic-negative'
 
 export const TextInput = ({
@@ -25,22 +26,25 @@ export const TextInput = ({
   fullWidth = false,
   ...props
 }: TextInputProps): JSX.Element => {
+  const textInputStyle = twMerge(
+    baseStyles,
+    clsx(
+      {
+        [errorStyles]: error,
+        'w-full': fullWidth,
+        'pl-48': startAdornment,
+        'pr-48': endAdornment,
+      },
+      className
+    )
+  )
+
   return (
     <div className='relative'>
       {startAdornment && (
         <span className='absolute left-14 top-10'>{startAdornment}</span>
       )}
-      <input
-        {...register}
-        type={type}
-        className={clsx(
-          baseStyles,
-          error ? errorStyles : 'border-gray-200',
-          fullWidth && 'w-full',
-          className
-        )}
-        {...props}
-      />
+      <input {...register} type={type} className={textInputStyle} {...props} />
       {endAdornment && (
         <span className='absolute right-14 top-14'>{endAdornment}</span>
       )}
