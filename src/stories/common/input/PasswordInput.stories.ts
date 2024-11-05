b93e1025b389e8ce2a99925acaadb2247bde054b
binyongbin <kyb2475@gmail.com>
2024-11-06 00:22:10 +0900

    [#43] ♻️ Update PasswordInput Storybook to ensure icon displays correctly inside input field

--- src/stories/common/input/PasswordInput.stories.ts
+++ src/stories/common/input/PasswordInput.stories.ts
@@ -6,10 +6,11 @@ export default {
   title: 'Example/PasswordInput',
   component: PasswordInput,
   parameters: {
-    layout: 'centered',
+    layout: 'fullscreen',
   },
   tags: ['input', 'password'],
   argTypes: {
+    error: { control: 'boolean' },
     fullWidth: { control: 'boolean' },
   },
   args: {
@@ -19,20 +20,14 @@ export default {
 
 export const Default = {
   args: {
-    // placeholder: '비밀번호를 입력하세요.',
+    placeholder: '비밀번호를 입력하세요.',
+    fullWidth: true,
   },
 }
 
 export const Error = {
   args: {
     error: true,
-    // placeholder: '비밀번호가 올바르지 않습니다.',
-  },
-}
-
-export const FullWidth = {
-  args: {
     fullWidth: true,
-    // placeholder: 'fullWidth 상태의 패스워드 인풋입니다.',
   },
 }
