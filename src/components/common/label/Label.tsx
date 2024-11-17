0f5fbf0fde883263dcf5212c72f2c89599dede07
binyongbin <kyb2475@gmail.com>
2024-11-17 21:38:03 +0900

    [#47] ✨ Implement Label component

--- src/components/common/label/Label.tsx
+++ src/components/common/label/Label.tsx
@@ -1,3 +1,28 @@
-export const Label = () => {
-  return <label></label>
+import clsx from 'clsx'
+
+export interface LabelProps
+  extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  required?: boolean
+  labelText?: string
+}
+
+export const Label = ({
+  required = false,
+  labelText,
+  htmlFor,
+  children,
+  className = '',
+}: LabelProps) => {
+  const labelClass = clsx('flex flex-col', className)
+  const requiredClass = 'text-body3 font-medium text-primary-normal'
+
+  return (
+    <label htmlFor={htmlFor} className={labelClass}>
+      <div className='mb-4 flex items-center'>
+        <span className='font-medium text-gray-600'>{labelText}</span>
+        {required && <span className={requiredClass}>*</span>}
+      </div>
+      {children}
+    </label>
+  )
 }
