a47c6b1abb2e2be7ebdac7e83f510c72d4883bd4
장아영 <jang-ayoung@jang-ayeong-ui-iMac.local>
2024-11-17 22:14:42 +0900

    [#56] ✨ root Page for test (for logout)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import { useAuthStore } from '@/lib/store/auth'
+import axios from 'axios'
+
 export default function Home(): JSX.Element {
-  return <>홈페이지</>
+  const { logout } = useAuthStore()
+  const router = useRouter()
+
+  const handleLogout = () => {
+    logout()
+    ;async () => {
+      await axios.post('/v1/auth/sign-in', {}, { withCredentials: true })
+    }
+    router.push('/sign-in')
+    console.log('로그아웃 성공')
+  }
+
+  return (
+    <>
+      <div>홈페이지</div>
+      <button onClick={handleLogout}>로그아웃</button>
+    </>
+  )
 }
