2c04d4b5278a2a816f771651bac08fae4c00c75e
장아영 <jang-ayoung@jang-ayeong-ui-iMac.local>
2024-11-17 22:12:32 +0900

    [#56] ✨ add signIn query (use 'axios')

---
+++ src/lib/api/auth.ts
@@ -0,0 +1,29 @@
+import { SignInReponse, SignInRequest } from '@/types/login.types'
+import axios from 'axios'
+
+// 환경 변수에서 API BASE URL 가져오기
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+
+if (!BASE_URL) {
+  throw new Error("환경 변수 'NEXT_PUBLIC_API_BASE_URL'이 정의되지 않았습니다.")
+}
+
+// Axios 인스턴스 생성
+const apiClient = axios.create({
+  baseURL: BASE_URL,
+//   withCredentials: true,
+})
+
+// 로그인 요청 처리
+export const signIn = async (data: SignInRequest): Promise<SignInReponse> => {
+  try {
+    const response = await apiClient.post('/v1/auth/sign-in', data, {
+    //   withCredentials: true,
+    })
+    console.log('로그인 성공:', response.data)
+    return response.data
+  } catch (error: any) {
+    console.error('로그인 실패:', error.response?.data || error.message)
+    throw error // 에러를 호출자에게 전달
+  }
+}
