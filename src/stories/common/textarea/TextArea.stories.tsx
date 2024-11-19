722b448d1a88ecd207214715814e4878ef293528
binyongbin <kyb2475@gmail.com>
2024-11-19 22:56:30 +0900

    [#57] ✅ Refactor to remove useArgs hook

--- src/stories/common/textarea/TextArea.stories.tsx
+++ src/stories/common/textarea/TextArea.stories.tsx
@@ -1,16 +1,14 @@
-import { useArgs } from '@storybook/preview-api'
-import { Meta, StoryFn } from '@storybook/react'
+import { fn } from '@storybook/test'
 
-import { TextArea, TextAreaProps } from '@/components/common/textarea/TextArea'
+import { TextArea } from '@/components/common/textarea/TextArea'
 
 export default {
   title: 'Common/TextArea/TextArea',
   component: TextArea,
   parameters: {
-    layout: 'fullscreen',
+    layout: 'padded',
   },
   argTypes: {
-    value: { control: 'text' },
     placeholder: { control: 'text' },
     className: { control: 'text' },
     size: {
@@ -19,42 +17,28 @@ export default {
     },
     fullWidth: { control: 'boolean' },
   },
-} as Meta
-
-const Template: StoryFn<TextAreaProps> = (args: TextAreaProps) => {
-  const [{ value }, updateArgs] = useArgs()
-
-  return (
-    <div className='flex w-1200 justify-center p-30'>
-      <TextArea
-        {...args}
-        value={value}
-        onChange={e => updateArgs({ value: e.target.value })}
-      />
-    </div>
-  )
+  args: { onChange: fn() },
 }
 
-export const Small = Template.bind({})
-Small.args = {
-  value: '',
-  placeholder: '댓글을 입력해보세요!',
-  size: 'sm',
-  fullWidth: true,
+export const Small = {
+  args: {
+    placeholder: '댓글을 입력해보세요!',
+    size: 'sm',
+    fullWidth: true,
+  },
 }
 
-export const Medium = Template.bind({})
-Medium.args = {
-  value: '',
-  placeholder: '주요 업무를 입력해주세요.',
-  size: 'md',
-  fullWidth: true,
+export const Medium = {
+  args: {
+    placeholder: '주요 업무를 입력해주세요.',
+    size: 'md',
+    fullWidth: true,
+  },
 }
-
-export const Large = Template.bind({})
-Large.args = {
-  value: '',
-  placeholder: '간단한 소개글을 작성해보세요!',
-  size: 'lg',
-  fullWidth: true,
+export const Large = {
+  args: {
+    placeholder: '간단한 소개글을 작성해주세요.',
+    size: 'lg',
+    fullWidth: true,
+  },
 }
