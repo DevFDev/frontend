a05f2a547743a633fafbfa419597c4c0b01706e3
binyongbin <kyb2475@gmail.com>
2024-11-19 22:25:15 +0900

    [#57] ✨ Implement TextArea component

---
+++ src/components/common/textarea/TextArea.tsx
@@ -0,0 +1,42 @@
+import clsx from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export interface TextAreaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  fullWidth?: boolean
+  size?: 'sm' | 'md' | 'lg'
+}
+
+const baseStyles =
+  'text-body-1 resize-none rounded-12 border-1 border-gray-200 p-12 font-medium text-gray-800 placeholder:text-gray-500'
+
+export const TextArea = ({
+  fullWidth = false,
+  size = 'sm',
+  value,
+  placeholder,
+  className = '',
+  ...props
+}: TextAreaProps): JSX.Element => {
+  const textAreaClass = twMerge(
+    baseStyles,
+    clsx(
+      {
+        'w-full': fullWidth,
+        'h-98': size === 'sm',
+        'h-104': size === 'md',
+        'h-140': size === 'lg',
+      },
+      className
+    )
+  )
+  return (
+    <textarea
+      value={value}
+      aria-label='textarea'
+      className={textAreaClass}
+      placeholder={placeholder}
+      {...props}
+    />
+  )
+}
