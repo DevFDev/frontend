4b381433b5b91301a90be24ce85f2e4eb88cb950
binyongbin <kyb2475@gmail.com>
2024-11-21 00:02:29 +0900

    [#57] ♻️ Apply forwardRef and remove aria-label

--- src/components/common/textarea/TextArea.tsx
+++ src/components/common/textarea/TextArea.tsx
@@ -1,3 +1,5 @@
+import { forwardRef } from 'react'
+
 import clsx from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
@@ -10,33 +12,39 @@ export interface TextAreaProps
 const baseStyles =
   'text-body-1 resize-none rounded-12 border-1 border-gray-200 p-12 font-medium text-gray-800 placeholder:text-gray-500'
 
-export const TextArea = ({
-  fullWidth = false,
-  size = 'md',
-  value,
-  placeholder,
-  className = '',
-  ...props
-}: TextAreaProps): JSX.Element => {
-  const textAreaClass = twMerge(
-    baseStyles,
-    clsx(
-      {
-        'w-full': fullWidth,
-        'h-100': size === 'sm',
-        'h-104': size === 'md',
-        'h-140': size === 'lg',
-      },
-      className
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  (
+    {
+      fullWidth = false,
+      size = 'md',
+      value,
+      placeholder,
+      className = '',
+      ...props
+    },
+    ref
+  ): JSX.Element => {
+    const textAreaClass = twMerge(
+      baseStyles,
+      clsx(
+        {
+          'w-full': fullWidth,
+          'h-100': size === 'sm',
+          'h-104': size === 'md',
+          'h-140': size === 'lg',
+        },
+        className
+      )
     )
-  )
-  return (
-    <textarea
-      value={value}
-      aria-label='textarea'
-      className={textAreaClass}
-      placeholder={placeholder}
-      {...props}
-    />
-  )
-}
+    return (
+      <textarea
+        ref={ref}
+        value={value}
+        className={textAreaClass}
+        placeholder={placeholder}
+        {...props}
+      />
+    )
+  }
+)
+TextArea.displayName = 'TextArea'
