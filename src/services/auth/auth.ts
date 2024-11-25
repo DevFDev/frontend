71bfd66eca67eb26b32890fc49bcf181cdb13d68
장아영 <jang-ayoung@jang-ayeong-ui-iMac.local>
2024-11-25 17:10:45 +0900

    [#120] 🗑️ remove unused fetch function

--- src/services/auth/auth.ts
+++ src/services/auth/auth.ts
@@ -1,30 +1,5 @@
 import { SignInRequest, SignInResponse, SignUpRequest} from '@/types/auth.types'
-import axios, { AxiosResponse } from 'axios'
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
-
-if (!BASE_URL) {
-  throw new Error("환경 변수 'NEXT_PUBLIC_API_BASE_URL'이 정의되지 않았습니다.")
-}
-
-// Axios 인스턴스 생성
-const apiClient = axios.create({
-  baseURL: BASE_URL,
-  //   withCredentials: true,
-})
-
-export const signIn = async (
-  data: SignInRequest
-): Promise<AxiosResponse<SignInResponse>> => {
-  return await apiClient.post<SignInResponse>('/v1/auth/sign-in', data, {
-    // withCredentials: true, //
-  })
-}
-
-export const signOut = async () => {
-  return await axios.post('/v1/auth/logout', {}, { withCredentials: true })
-}
- 
 const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL
 export const SignUp = async (data: SignUpRequest): Promise<Response> => {
   return await fetch(`${BACKEND_BASE_URL}/v1/auth/sign-up`, {
