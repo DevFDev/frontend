d92a13a8e7a9fa71ee5cc8070e062a93abdaa62d
KingNono1030 <hoejy0821@gmail.com>
2024-11-26 00:42:42 +0900

    [#60] ✨ update auth related types

--- src/types/auth.types.d.ts
+++ src/types/auth.types.d.ts
@@ -1,18 +1,32 @@
+/*
+path: '/v1/auth'
+GET: 로그인한 유저 조회
+*/
+export type GetMemberInfoResult = User
+
+/*
+path: '/v1/auth/sign-up'
+POST: 회원가입
+*/
 export interface SignUpRequest {
   email: Email
   password: Password
   name: Name
-  gitHub?: GitHub
+  gitHub: GitHub
+}
+export interface SignUpResponse extends User {
+  gitHub: GitHub
 }
 
+/*
+path: '/v1/auth/sign-in'
+POST: 로그인
+*/
 export interface SignInRequest {
   email: Email
   password: Password
 }
-
 export interface SignInResponseResult extends User {
   accessToken: Token
   refreshToken: Token
 }
-
-export type SignInResponse = ApiResponse<SignInResponseResult>
