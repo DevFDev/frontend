8748e54efb7e942a043add315114862c9e225eb1
장아영 <jang-ayoung@jang-ayeong-ui-iMac.local>
2024-11-26 20:30:03 +0900

    [#128] ♻️ create 'SignIn', 'SignOut' fetch function

--- src/services/auth/auth.ts
+++ src/services/auth/auth.ts
@@ -1,4 +1,8 @@
-import { SignInRequest, SignInResponse, SignUpRequest} from '@/types/auth.types'
+import {
+  SignInRequest,
+  SignInResponse,
+  SignUpRequest,
+} from '@/types/auth.types'
 
 const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_BASE_URL
 export const SignUp = async (data: SignUpRequest): Promise<Response> => {
@@ -10,4 +14,18 @@ export const SignUp = async (data: SignUpRequest): Promise<Response> => {
     body: JSON.stringify(data),
   })
 }
- 
+
+export const SignIn = async (data: SignInRequest): Promise<Response> => {
+  return await fetch(`/api/auth/sign-in`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  })
+}
+
+export const SignOut = async () => {
+  return await fetch(`/api/auth/sign-out`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  })
+}
