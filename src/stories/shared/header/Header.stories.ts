b78b624451dd3658d5231228ef0774538ec366f7
KingNono1030 <hoejy0821@gmail.com>
2024-12-03 22:41:07 +0900

    [#103] ✅ add Header Stories

---
+++ src/stories/shared/header/Header.stories.ts
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Header } from '@/components/shared/header'
+
+export default {
+  title: 'Shared/Header/Header',
+  component: Header,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as Meta
+
+export const Default: StoryObj = {
+  args: {
+    isAuthenticated: false,
+    user: null,
+  },
+}
+export const WithUserMenu: StoryObj = {
+  args: {
+    isAuthenticated: true,
+    user: {
+      id: '2',
+      name: 'Alexander',
+      imageUrl: 'https://picsum.photos/250/250',
+    },
+  },
+}
+export const LoggedInWithoutUser: StoryObj = {
+  args: {
+    isAuthenticated: true,
+    user: null,
+  },
+}
