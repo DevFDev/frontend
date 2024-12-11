b35b86de70187308c540ca64a4823f7952b528c1
장아영 <jang-ayoung@jang-ayeong-ui-iMac.local>
2024-12-11 15:19:20 +0900

    [#141] ♻️ apply new types

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,48 +14,6 @@ export const config = {
 export async function middleware(
   req: NextRequest
 ): Promise<NextResponse<unknown>> {
-  // function parseCookies(cookieHeader: string | null): Record<string, string> {
-  //   if (!cookieHeader) return {}
-  //   return cookieHeader
-  //     .split(';')
-  //     .reduce((acc: Record<string, string>, cookie) => {
-  //       const [key, ...values] = cookie.trim().split('=')
-  //       acc[key] = decodeURIComponent(values.join('='))
-  //       return acc
-  //     }, {})
-  // }
-  // const cookieHeader = req.headers.get('cookie');
-  // console.log('Cookie Header:', cookieHeader);
-
-  // const accessToken = cookieHeader
-  //   ?.split('; ')
-  //   .find(cookie => cookie.startsWith('accessToken='))
-  //   ?.split('=')[1];
-
-  // const refreshToken = cookieHeader
-  //   ?.split('; ')
-  //   .find(cookie => cookie.startsWith('refreshToken='))
-  //   ?.split('=')[1];
-
-  // console.log('Access Token:', accessToken);
-  // console.log('Refresh Token:', refreshToken);
-
-  // function parseCookies(cookieHeader: string | null): Record<string, string> {
-  //   if (!cookieHeader) return {};
-  //   return cookieHeader
-  //     .split(';')
-  //     .reduce((acc: Record<string, string>, cookie) => {
-  //       const [key, ...values] = cookie.trim().split('=');
-  //       acc[key] = decodeURIComponent(values.join('='));
-  //       return acc;
-  //     }, {});
-  // }
-
-  // const cookieHeader = req.headers.get('cookie');
-  // const cookies = parseCookies(cookieHeader);
-  // const accessToken = cookies['accessToken'];
-  // const refreshToken = cookies['refreshToken'];
-
   const cookies = req.cookies
   const accessToken = cookies.get('accessToken')?.value
   const refreshToken = cookies.get('refreshToken')?.value
@@ -68,17 +26,15 @@ export async function middleware(
     if (!refreshToken) {
       console.log('Refresh Token is also missing. Redirecting to /sign-in')
       return NextResponse.redirect(new URL('/sign-in', req.url))
-      // return NextResponse.next()
     }
     try {
-      type RefreshToken = { accessToken: string }
       const { result } = await backendApi
         .post(`v1/auth/new-token`, {
           json: { oldAccessToken: accessToken, refreshToken },
         })
-        .json<ApiResponse<RefreshToken>>()
+        .json<ApiResponse<AccessTokenResponse>>()
 
-      const newAccessToken = result?.accessToken
+      const newAccessToken = result.accessToken
 
       console.log('엑세스 토큰이 성공적으로 갱신되었습니다', newAccessToken)
 
