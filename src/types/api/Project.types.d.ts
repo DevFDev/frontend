6bd9d83c563238270afd98ee949a4a9f3876073a
KingNono1030 <hoejy0821@gmail.com>
2024-12-16 16:02:21 +0900

    [#60] 🔧 wrap create project interface with MultipartFormData

--- src/types/api/Project.types.d.ts
+++ src/types/api/Project.types.d.ts
@@ -12,7 +12,7 @@ type ProjectBase = {
 - path: '/v1/project'
 - POST: 프로젝트 글 등록
 */
-export type CreateProjectRequest = ProjectBase
+export type CreateProjectRequest = MultipartFormData<ProjectBase>
 export interface CreateProjectResponse extends ProjectBase, TimeStamps {
   id: Id
   writer: MemberInfo
