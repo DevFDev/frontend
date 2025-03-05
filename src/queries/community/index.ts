a992c9a19c6e1205a61b6f6136dba22f0d5221e5
KingNono1030 <hoejy0821@gmail.com>
2025-03-05 20:58:11 +0900

    [#252] ✨ community list api

---
+++ src/queries/community/index.ts
@@ -0,0 +1,173 @@
+import { useRouter } from 'next/navigation'
+
+import { ApiResponse } from '@/types/api/ApiResponse.types'
+import {
+  GetCommunityListQuery,
+  GetCommunityListResponse,
+} from '@/types/api/Community.types'
+import {
+  AddTeamMemberRequest,
+  AddTeamMemberResponse,
+  CreateTeamRecruitmentRequest,
+  CreateTeamRecruitmentResponse,
+  GetTeamMembersResponse,
+  GetTeamRecruitmentListQuery,
+  GetTeamRecruitmentListResponse,
+  GetTeamRecruitmentResponse,
+  SearchMembersResponse,
+  UpdateTeamRecruitmentRequest,
+  UpdateTeamRecruitmentResponse,
+} from '@/types/api/Team.types'
+import {
+  UseMutationResult,
+  UseQueryResult,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query'
+
+import { getCommunityList } from '@/services/community'
+
+// 커뮤니티 게시글 목록 조회
+export const useCommunityRecruitmentList = (
+  queries: GetCommunityListQuery
+): UseQueryResult<ApiResponse<GetCommunityListResponse>, Error> => {
+  return useQuery({
+    queryKey: [
+      'community',
+      queries?.searchTerm || '',
+      queries?.category || '',
+      queries?.sortBy || '',
+    ],
+    queryFn: async () => getCommunityList(queries),
+  })
+}
+
+// // 팀 모집글 상세 조회
+// export const useTeamRecruitment = (
+//   teamId: Id
+// ): UseQueryResult<GetTeamRecruitmentResponse, Error> => {
+//   const queryClient = useQueryClient()
+//   return useQuery({
+//     queryKey: ['teamRecruitment', teamId],
+//     queryFn: async () => {
+//       const { result } = await getTeamRecruitment(teamId)
+//       return result
+//     },
+//     initialData: queryClient.getQueryData(['teamRecruitment', teamId]),
+//   })
+// }
+
+// // 팀 멤버 목록 조회
+// export const useTeamMembers = (
+//   teamId: Id
+// ): UseQueryResult<GetTeamMembersResponse, Error> => {
+//   return useQuery({
+//     queryKey: ['teamMembers', teamId],
+//     queryFn: async () => {
+//       const { result } = await getTeamMembers(teamId)
+//       return result
+//     },
+//   })
+// }
+
+// // 팀 모집글 생성
+// export const useCreateTeamRecruitment = (): UseMutationResult<
+//   ApiResponse<CreateTeamRecruitmentResponse>,
+//   Error,
+//   CreateTeamRecruitmentRequest
+// > => {
+//   const router = useRouter()
+//   return useMutation({
+//     mutationFn: createTeamRecruitment,
+//     onSuccess: ({ result }) => {
+//       router.push(`/team/${result.id}`)
+//     },
+//   })
+// }
+
+// // 팀 모집글 수정
+// export const useUpdateTeamRecruitment = (
+//   teamId: Id
+// ): UseMutationResult<
+//   ApiResponse<UpdateTeamRecruitmentResponse>,
+//   Error,
+//   UpdateTeamRecruitmentRequest
+// > => {
+//   const router = useRouter()
+//   const queryClient = useQueryClient()
+//   return useMutation({
+//     mutationFn: data => updateTeamRecruitment(teamId, data),
+//     onSuccess: ({ result }) => {
+//       queryClient.invalidateQueries({ queryKey: ['teamRecruitments', teamId] })
+
+//       router.push(`/team/${result.id}`)
+//     },
+//   })
+// }
+
+// // 팀원 추가
+// export const useAddTeamMember = (
+//   teamId: Id
+// ): UseMutationResult<
+//   ApiResponse<AddTeamMemberResponse>,
+//   Error,
+//   AddTeamMemberRequest
+// > => {
+//   return useMutation({
+//     mutationFn: data => addTeamMember(teamId, data),
+//   })
+// }
+
+// // 팀원 삭제
+// export const useDeleteTeamMember = (
+//   teamId: Id
+// ): UseMutationResult<ApiResponse, Error, Id> => {
+//   return useMutation({
+//     mutationFn: memberId => deleteTeamMember(teamId, memberId),
+//   })
+// }
+
+// // 팀 모집글 삭제
+// export const useDeleteTeamRecruitment = (
+//   teamId: Id
+// ): UseMutationResult<ApiResponse, Error, Id> => {
+//   const router = useRouter()
+//   const queryClient = useQueryClient()
+//   return useMutation({
+//     mutationFn: () => deleteTeamRecruitment(teamId),
+//     onSuccess: () => {
+//       router.push('/team')
+//       queryClient.invalidateQueries({ queryKey: ['teamRecruitments'] })
+//     },
+//     onError: error => console.error(error),
+//   })
+// }
+
+// // 팀 모집 마감
+// export const useCloseTeamRecruitment = (
+//   teamId: Id
+// ): UseMutationResult<ApiResponse, Error, Id> => {
+//   const queryClient = useQueryClient()
+//   return useMutation({
+//     mutationFn: () => closeTeamRecruitment(teamId),
+//     onSuccess: () => {
+//       queryClient.invalidateQueries({ queryKey: ['teamRecruitments', teamId] })
+//     },
+//   })
+// }
+
+// // 멤버 검색
+// export const useSearchMembers = (
+//   teamId: Id,
+//   nickname?: string
+// ): UseQueryResult<SearchMembersResponse, Error> => {
+//   return useQuery({
+//     queryKey: ['teamMembers', teamId, 'search', nickname],
+//     queryFn: async () => {
+//       const { result } = await searchMembers(teamId, nickname)
+//       return result
+//     },
+//     enabled: !!teamId, // teamId가 있을 때만 쿼리 실행
+//   })
+// }
---
+++ src/services/community/index.ts
@@ -0,0 +1,95 @@
+import { ApiResponse } from '@/types/api/ApiResponse.types'
+import {
+  GetCommunityListQuery,
+  GetCommunityListResponse,
+} from '@/types/api/Community.types'
+
+import { backendApi } from '@/services/api'
+
+//커뮤니티 게시글 전체 조회
+// searchTerm, category, sortBy 는 쿼리 스트링
+export const getCommunityList = async ({
+  searchTerm,
+  category,
+  sortBy,
+}: GetCommunityListQuery): Promise<ApiResponse<GetCommunityListResponse>> => {
+  return await backendApi
+    .get('v1/community', {
+      searchParams: {
+        searchTerm: searchTerm || '',
+        category,
+        sortBy,
+      },
+    })
+    .json()
+}
+
+// //팀 모집글 상세 조회 (여기에서의 id 는 게시글 고유 id 이자 해당 팀 id -> teamId?)
+// export const getTeamRecruitment = async (
+//   teamId: Id
+// ): Promise<ApiResponse<GetTeamRecruitmentResponse>> => {
+//   return await backendApi.get(`v1/team/${teamId}`).json()
+// }
+
+// //팀 멤버 전체 조회 (teamId : 팀 Id이자, 모집글 id)
+// export const getTeamMembers = async (
+//   teamId: Id
+// ): Promise<ApiResponse<GetTeamMembersResponse>> => {
+//   return await backendApi.get(`v1/team/${teamId}/members`).json()
+// }
+
+// //맴버 리스트 검색 (teamId : 팀 Id) -> 전체 멤버를 조회할 수 있도록 (nickname을 입력하지 않고도)
+// export const searchMembers = async (
+//   teamId: Id,
+//   nickname?: string
+// ): Promise<ApiResponse<SearchMembersResponse>> => {
+//   const url = nickname
+//     ? `v1/team/${teamId}/search-members?nickname=${nickname}`
+//     : `v1/team/${teamId}/search-members`
+//   return await backendApi.get(url).json()
+// }
+
+// //팀 모집글 등록
+// export const createTeamRecruitment = async (
+//   data: CreateTeamRecruitmentRequest
+// ): Promise<ApiResponse<CreateTeamRecruitmentResponse>> => {
+//   return await backendApi.post('v1/team', { json: data }).json()
+// }
+
+// //팀 멤버 추가
+// export const addTeamMember = async (
+//   teamId: Id,
+//   data: AddTeamMemberRequest
+// ): Promise<ApiResponse<AddTeamMemberResponse>> => {
+//   return await backendApi.post(`v1/team/${teamId}/add`, { json: data }).json()
+// }
+
+// //팀원 모집 마감
+// export const closeTeamRecruitment = async (
+//   teamId: Id
+// ): Promise<ApiResponse> => {
+//   return await backendApi.patch(`v1/team/${teamId}/close`).json()
+// }
+
+// //팀원 모집글 삭제
+// export const deleteTeamRecruitment = async (
+//   teamId: Id
+// ): Promise<ApiResponse> => {
+//   return await backendApi.delete(`v1/team/${teamId}`).json()
+// }
+
+// //팀 멤버 삭제
+// export const deleteTeamMember = async (
+//   teamId: Id,
+//   memberId: Id
+// ): Promise<ApiResponse> => {
+//   return await backendApi.delete(`v1/team/${teamId}/members/${memberId}`).json()
+// }
+
+// //팀 모집글 수정
+// export const updateTeamRecruitment = async (
+//   teamId: Id,
+//   data: UpdateTeamRecruitmentRequest
+// ): Promise<ApiResponse<UpdateTeamRecruitmentResponse>> => {
+//   return await backendApi.patch(`v1/team/${teamId}`, { json: data }).json()
+// }
